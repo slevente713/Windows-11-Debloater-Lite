45458b577815d0af3c1f7f60e4d19175276c4fc7
SLevente001 <221684507+slevente713@users.noreply.github.com>
2026-04-26 11:58:11 +0200

    Add English translations to user prompts in script and IE uninstaller

--- Debloater Lite.ps1
+++ Debloater Lite.ps1
@@ -1,40 +1,40 @@
-Write-Host "Rendszergazdaként futtattál? Ha nem akkor indíts újra rendszergazdaként!" -ForegroundColor Cyan
-Write-Host "FIGYELEM! A KÉPERNYŐ NÉHA PÁR MÁSODPERCRE ELSÖTÉTÜLHET, HOGY ÉRVÉNYESÜLJENEK AUTÓMATIKUSAN A MÓDOSÍTÁSOK!" -ForegroundColor Cyan
-Write-Host "FIGYELEM! A HIBAÜZENETEKET ZÁRD BE, AZOK CSAK AZT JELENTIK HOGY AZ ADOTT ALKALMAZÁS NINCS A GÉPEN!" -ForegroundColor Cyan
+Write-Host "Rendszergazdaként futtattál? Ha nem akkor indíts újra rendszergazdaként! Did you run me as an administrator? If not, run me again as an administrator!" -ForegroundColor Cyan
+Write-Host "FIGYELEM! A KÉPERNYŐ NÉHA PÁR MÁSODPERCRE ELSÖTÉTÜLHET, HOGY ÉRVÉNYESÜLJENEK AUTÓMATIKUSAN A MÓDOSÍTÁSOK! ATTENTION! THE SCREEN MAY GO DARK FOR A FEW SECONDS TO ALLOW THE CHANGES TO TAKE EFFECT AUTOMATICALLY!" -ForegroundColor Cyan
+Write-Host "FIGYELEM! A HIBAÜZENETEKET ZÁRD BE, AZOK CSAK AZT JELENTIK HOGY AZ ADOTT ALKALMAZÁS NINCS A GÉPEN! ATTENTION! IGNORE THE ERROR MESSAGES; THEY SIMPLY MEAN THAT THE APPLICATION IS NOT INSTALLED ON YOUR COMPUTER!" -ForegroundColor Cyan
 Set-ExecutionPolicy Unrestricted -Force
 winget source update --silent
-Write-Host "Winget források frissítése..." -ForegroundColor Cyan
+Write-Host "Winget források frissítése... Update Winget sources" -ForegroundColor Cyan
 winget uninstall --id Microsoft.GetHelp_8wekyb3d8bbwe --silent
-Write-Host "Segítség kérése törlése..." -ForegroundColor Cyan
+Write-Host "Segítség kérése törlése... Removing Get Help..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe --silent
-Write-Host "Visszajelzés küldése törlése..." -ForegroundColor Cyan..." -ForegroundColor Cyan
+Write-Host "Visszajelzés küldése törlése... Removing Feedback Hub..." -ForegroundColor Cyan..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.DevHome_8wekyb3d8bbwe --silent
-Write-Host "Dev Home törlése..." -ForegroundColor Cyan
-winget uninstall --id Microsoft.549981C327F10_8wekyb3d8bbwe --silent
-Write-Host "Cortana törlése..." -ForegroundColor Cyan
+Write-Host "Dev Home törlése... Removing Dev Home..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.Todos_8wekyb3d8bbwe --silent
-Write-Host "To Do törlése..." -ForegroundColor Cyan
+Write-Host "To Do törlése... Removing To Do..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.ZuneVideo_8wekyb3d8bbwe --silent
-Write-Host "Filmek+TV törlése..." -ForegroundColor Cyan
+Write-Host "Filmek+TV törlése... Removing Movies and TV..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.BingWeather_8wekyb3d8bbwe --silent
-Write-Host "Időjárás törlése..." -ForegroundColor Cyan
+Write-Host "Időjárás törlése... Removing Bing Weather..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.BingNews_8wekyb3d8bbwe --silent
-Write-Host "Hírek törlése..." -ForegroundColor Cyan
+Write-Host "Hírek törlése... Removing Bing News..." -ForegroundColor Cyan
+dism /Online /Disable-Feature /FeatureName:Internet-Explorer-Optional-amd64 /NoRestart
+Write-Host "Internegt Explorer 11 törlése... Removing Internet Explorer 11..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.BingSports_8wekyb3d8bbwe --silent
-Write-Host "Sportok törlése..." -ForegroundColor Cyan
+Write-Host "Sportok törlése... Removing Bing Sports..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.BingFinance_8wekyb3d8bbwe --silent
-Write-Host "Finanszírozás törlése..." -ForegroundColor Cyan
+Write-Host "Finanszírozás törlése... Removing Bing Finance..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.SkypeApp_kzf8qxf38zg5c --silent
-Write-Host "Skype törlése..." -ForegroundColor Cyan
+Write-Host "Skype törlése... Removing Skype..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.WindowsSoundRecorder_8wekyb3d8bbwe --silent
-Write-Host "Hangrögzítő törlése..." -ForegroundColor Cyan
+Write-Host "Hangrögzítő törlése... Removing Sound Recorder..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.MSPaint3D_8wekyb3d8bbwe --silent 
-Write-Host "Paint 3D törlése..." -ForegroundColor Cyan
+Write-Host "Paint 3D törlése... Removing Paint 3D..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.XPSViewer_8wekyb3d8bbwe --silent
 dism /online /disable-feature /featurename:XPS.Viewer /norestart
-Write-Host "XPS Megjelenítő törlése..." -ForegroundColor Cyan
+Write-Host "XPS Megjelenítő törlése... Removing XPS Viewer..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.549981C32F101_8wekyb3d8bbwe --silent
-Write-Host "Cortana törlése..." -ForegroundColor Cyan
+Write-Host "Cortana törlése... Uninstalling Cortana..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.Copilot_8wekyb3d8bbwe --silent
 $policyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsCopilot"
 if (!(Test-Path $policyPath)) { New-Item -Path $policyPath -Force | Out-Null }
@@ -50,32 +50,36 @@ New-ItemProperty -Path $userPolicyPath -Name "TurnOffWindowsCopilot" -PropertyTy
 Stop-Process -Name explorer -Force; Start-Process explorer.exe
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowCopilotButton" -PropertyType DWord -Value 0 -Force | Out-Null
 Stop-Process -Name explorer -Force; Start-Process explorer.exe
-Write-Host "Copilot törlése..." -ForegroundColor Cyan
+Write-Host "Copilot törlése... Removing Copilot..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.Wallet_8wekyb3d8bbwe --silent
-Write-Host "Pénztárca törlése..." -ForegroundColor Cyan
+Write-Host "Pénztárca törlése... Removing Bing Wallet..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe --silent
-Write-Host "Sticky Notes (Cetlik) törlése..." -ForegroundColor Cyan
+Write-Host "Sticky Notes (Cetlik) törlése... Removing Sticky Notes (Cetlik)..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.Microsoft3DViewer_8wekyb3d8bbwe --silent
-Write-Host "3D megjelenítő törlése..." -ForegroundColor Cyan
+Write-Host "3D megjelenítő törlése... Removing 3D Viewer..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe --silent
-Write-Host "Microsoft Solitare Collection törlése..." -ForegroundColor Cyan
+Write-Host "Microsoft Solitare Collection törlése... Removing Microsoft Solitare Collection..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.People_8wekyb3d8bbwe --silent
-Write-Host "Névjegyek alkalmazás törlése... (A NÉVJEGYEID AZ OUTLOOKBAN MEGMARADNAK!)" -ForegroundColor Cyan
+Write-Host "Névjegyek alkalmazás törlése... (A NÉVJEGYEID AZ OUTLOOKBAN MEGMARADNAK!) Removing People... (THIS SCRIPT KEEPS YOUR CONTACTS IN OUTLOOK!)" -ForegroundColor Cyan
 winget uninstall --id Microsoft.MixedReality.Portal_8wekyb3d8bbwe --silent
-Write-Host "Mixed Reality Portal törlése..." -ForegroundColor Cyan
+Write-Host "Mixed Reality Portal törlése... Removing Mixed Reality Portal..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.WindowsMaps_8wekyb3d8bbwe --silent
-Write-Host "Térkép törlése..." -ForegroundColor Cyan
+Write-Host "Térkép törlése... Removing Bing Maps..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.Getstarted_8wekyb3d8bbwe --silent
-Write-Host "Tippek alkalmazás törlése..." -ForegroundColor Cyan
+Write-Host "Tippek alkalmazás törlése... Removing Tips" -ForegroundColor Cyan
 winget uninstall --id Microsoft.Family_8wekyb3d8bbwe --silent
-Write-Host "Microsoft Család alkalmazás törlése..." -ForegroundColor Cyan
+Write-Host "Microsoft Család alkalmazás törlése... Removing Microsoft Family..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.Clipchamp.Clipchamp_8wekyb3d8bbwe --silent
-Write-Host "Clipchamp és Movie Maker törlése..." -ForegroundColor Cyan
+Write-Host "Clipchamp törlése... Removing Clipchamp..." -ForegroundColor Cyan
 winget uninstall --id Microsoft.WindowsCamera_8wekyb3d8bbwe --silent
-Write-Host "Windows Kamera törlése..." -ForegroundColor Cyan
+Write-Host "Windows Kamera törlése... Removing Windows Camera..." -ForegroundColor Cyan
 Disable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-WordPad" -NoRestart
-Write-Host "Wordpad törlése..." -ForegroundColor Cyan
+Write-Host "Wordpad törlése... Removing Wordpad" -ForegroundColor Cyan
 Disable-WindowsOptionalFeature -Online -FeatureName "Printing-Foundation-Features" -NoRestart
-Write-Host "Fax+Scan letiltása..." -ForegroundColor Cyan
+Write-Host "Nyomtatási szolgáltatás törlése... Removing the Printer Foundation feature..." -ForegroundColor Cyan
+dism /Online /Disable-Feature /FeatureName:FaxServicesClientPackage /NoRestart
+Write-Host "Fax és scan törlése... Removing Fax and scan..." -ForegroundColor Cyan
+dism /Online /Disable-Feature /FeatureName:Microsoft-Windows-StepsRecorder /NoRestart
+Write-Host "Problémarögzítő törlése... Removing Steps recorder..." -ForegroundColor Cyan
 Set-ExecutionPolicy Restricted -Force
-Write-Host "Végeztünk. Indítsd újra a gépet!" -ForegroundColor Cyan
+Write-Host "Végeztünk. Indítsd újra a gépet! We're done! Please restart your computer!" -ForegroundColor Cyan
